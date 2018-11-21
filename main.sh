b2b3c787ffadac715389fabf574ecdc01efdbae6
Edgar Albalate <dev.gardo@gmail.com>
2018-11-21 18:38:21 +0100

    Game finished, could be improved

--- main.sh
+++ main.sh
@@ -3,10 +3,32 @@
 COLORS="RYMA"
 STEP=0
 LINE=""
+SECRET="RYAR"
+
 
 while [ $STEP -lt 15 ]
 do
-    read -p "Enter_the_DEVICE_Bssid: " LINE
-    echo $LINE $STEP
+    read -p "Enter " LINE
+    WHITE=0
+    BLACK=0
+    for i in 0 1 2 3
+    do
+        if [ ${SECRET:i:1} = ${LINE:i:1} ];
+        then
+            WHITE=$((WHITE+1))
+        else
+            BLACK=$((BLACK+1))
+        fi
+        if [[ $WHITE -eq 4 ]];
+        then
+            echo "YOU WIN! ;)"
+            exit 0
+        fi
+    done
+    
     STEP=$((STEP+1))
+    echo "#"$STEP "W"$WHITE"B"$BLACK   $LINE
+    
 done
+
+echo "YOU LOSE T_T"
